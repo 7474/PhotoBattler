39c3cea02a728dd57cc37c972c235b03180fb017
koudenpa <koudenpa@hotmail.com>
2018-05-31 02:19:41 +0900

    Table entity のキー内容を備考した。

--- PhotoBattlerFunctionApp/Models/TableEntities.cs
+++ PhotoBattlerFunctionApp/Models/TableEntities.cs
@@ -7,12 +7,27 @@ using System.Threading.Tasks;
 
 namespace PhotoBattlerFunctionApp.Models
 {
-
+    /// <summary>
+    /// PartitionKey = Image source category.
+    /// RowKey = Non rule.
+    /// {
+    /// PartitionKey: "Amazon",
+    /// RowKey: "ASIN + URL Hash"
+    /// }
+    /// </summary>
     public class CreateImageFromUrlsEntity : TableEntity
     {
         public string Url { get; set; }
         public ICollection<string> Tags { get; set; }
     }
+    /// <summary>
+    /// PartitionKey = Item source category.
+    /// RowKey = ID.
+    /// {
+    /// PartitionKey: "Amazon",
+    /// RowKey: "ASIN"
+    /// }
+    /// </summary>
     public class Item : TableEntity
     {
         public string Name { get; set; }
