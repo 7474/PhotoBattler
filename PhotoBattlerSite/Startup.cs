73aa95ce80e11ac62e96800f00261f19b5eeddbb
koudenpa <koudenpa@hotmail.com>
2018-06-02 22:51:16 +0900

    サイトを静的ファイルのみにした。

--- PhotoBattlerSite/Startup.cs
+++ PhotoBattlerSite/Startup.cs
@@ -21,30 +21,12 @@ namespace PhotoBattlerSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
             app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
         }
     }
 }
